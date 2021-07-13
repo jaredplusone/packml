def731215d31ebeef65e972dd825107b09ee86e1
jaredplusone <87380322+jaredplusone@users.noreply.github.com>
2021-07-13 09:51:47 -0500

    Added parenthesis to print statements
    Fixed missing parenthesis. Was an issue when trying to use python3.

--- packml_gui/src/packml_gui/packml.py
+++ packml_gui/src/packml_gui/packml.py
@@ -56,8 +56,8 @@ class Packml(Plugin):
                       help="Put plugin in silent mode")
         args, unknowns = parser.parse_known_args(context.argv())
         if not args.quiet:
-            print 'arguments: ', args
-            print 'unknowns: ', unknowns
+            print('arguments: ', args)
+            print('unknowns: ', unknowns)
 
         # Create QWidget
         self._widget = QWidget()
